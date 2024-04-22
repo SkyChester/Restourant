29a8803cda2a4b7171ec71f07a1a08e4ab1c74eb
SkyChester <dmtkachuk61@gmail.com>
2024-04-22 23:12:58 +0300

    #

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ const menu = document.querySelector("#menu");
 burger.addEventListener("click", () => {
     menu.classList.toggle("disp");
 });
+
+let clock = document.querySelector(".clock");
+
+function time() {
+    let date = new Date();
+    let hours = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
+    let min = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+    let sec = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
+    clock.innerHTML = `${hours}:${min}:${sec}`;
+}
+
+setInterval(time, 500);
